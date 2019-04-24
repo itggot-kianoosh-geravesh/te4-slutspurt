e97dcaf3747bc6d1b1e382849f204f8972c4bc03
kian00sh <kianoosh.geravesh@itggot.se>
2019-04-24 18:59:47 +0200

    created backend and frontend biolerplate

---
+++ app-backend/routes/users.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+  res.json([{
+      id: 1,
+      name: "test1"
+    },
+    {
+      id: 2,
+      name: "test2"
+    },
+  ])
+});
+
+module.exports = router;
