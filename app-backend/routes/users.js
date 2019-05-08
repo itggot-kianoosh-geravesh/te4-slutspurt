3dc9c379316bbc3b409d950b71bbbc735a810569
kian00sh <kianoosh.geravesh@itggot.se>
2019-05-08 18:02:32 +0200

    fix npm-watch

--- app-backend/routes/users.js
+++ app-backend/routes/users.js
@@ -1,5 +1,25 @@
 var express = require('express');
 var router = express.Router();
+const {
+  Pool,
+  Client
+} = require('pg')
+const connectionString = 'postgressql://localhost:5432/xsidb'
+
+const client = new Client({
+  connectionString: connectionString,
+})
+
+client.connect().then(_ => {
+  console.log("Connected to database!");
+}).catch(err => {
+  console.log("Could not connect to the database!")
+  console.log(err)
+});
+client.query('SELECT * FROM users', (err, res) => {
+  err === null ? console.log(res) : console.log(err)
+  client.end();
+})
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
