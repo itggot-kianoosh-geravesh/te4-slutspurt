1c0d3d30f8f5ef64b1684482ce74ccbc178e2426
kian00sh <kianoosh.geravesh@itggot.se>
2019-05-08 15:58:36 +0200

    detail fix and test

--- app-backend/app-client/src/App.jsx
+++ app-backend/app-client/src/App.jsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    users: []
+  };
+  componentDidMount() {
+    fetch("/users")
+      .then(res => res.json())
+      .catch(err => console.log(err))
+      .then(users => this.setState({ users }));
+  }
+  render() {
+    let { users } = this.state;
+    return (
+      <div className="App">
+        <h1>USERS</h1>
+        <ul>
+          {users.map(user => (
+            <li key={user.id}>{user.name}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
